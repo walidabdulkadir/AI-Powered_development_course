69ec880c2478e2bfaa4d5e2556456d38bebe87e1
walidabdulkadir <yesufabdulkadir942@gmail.com>
2026-09-02 16:55:46 +0300

    feat(chatgpt-clone-backend): add Express routes and logger middleware

--- week 2/01.Chatgpt_Clone/Backend/index.js
+++ week 2/01.Chatgpt_Clone/Backend/index.js
@@ -1,5 +1,29 @@
 import express from "express";
 const app = express();
+
+function logger(req, res) {
+  const url = req.url;
+  const method = req.method;
+  console.log(url, method);
+  res.send("from logger middleware");
+}
+
+app.get("/", logger, (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/about", (req, res) => {
+  res.send("hello world from about");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send("hello world from about");
+});
+
+app.get("/conservation", (req, res) => {
+  res.send("hello world from about");
+});
+
 app.listen(5500, () => {
   console.log("Server is running at http://localhost:5500");
 });
